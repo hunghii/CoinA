d956f1fa299e0d239d528a25a2dd0a145edb625b
hiimhung <hungdm.hn@havaz.vn>
2021-03-05 10:39:59 +0700

    HomeScreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,19 +1,38 @@
 import React,{useState} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet,Image} from 'react-native';
 
+const image = require('../../../assets/images/Saly-1.png');
 const HomeScreen = () => {
     return (
-        <View>
-            
+        <View style={styles.container}>
+            <Image style={styles.image} source={image}/>
+            <Text style={styles.title}>Welcome to CoinA </Text>
+            <Text style={styles.subtitle}>Invest your virtual $100.000 and compete with others</Text>
         </View>
     )
 }
 const styles = StyleSheet.create({
- container:{
-        flex: 1,
-        justifyContent: 'center',
+    container:{
+        flex:1,
+        padding:20,
         alignItems:'center',
     },
+    image: {
+        height: '40%',
+        aspectRatio:1,
+    },
+    title: {
+        color: '#fff',
+        fontSize:20,
+        fontWeight:'bold',
+        marginTop: 20,
+        marginBottom: 15
+    },
+    subtitle: {
+        color: '#fff',
+        fontSize:20,
+        textAlign:'center'
+    }
 })
 
 export default HomeScreen
